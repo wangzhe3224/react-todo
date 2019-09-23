f42d17c1993ff11845b8086808aa55d6d3dfb909
Zhe Wang <zhe.wang@cirdancapital.com>
2019-09-23 11:48:10 +0100

    Updates

---
+++ precache-manifest.57a62cfc05a72213d5dae8e48e6aad67.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8bf816a4834d5fa6936d22f65a503ab",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "160de4746f2bf6e679ac",
+    "url": "/react-todo/static/js/2.8b33dd9e.chunk.js"
+  },
+  {
+    "revision": "f8378106dbb8b0c4f879",
+    "url": "/react-todo/static/js/main.4b481e39.chunk.js"
+  },
+  {
+    "revision": "31ba4ba914284ff60672",
+    "url": "/react-todo/static/js/runtime~main.0c4a585c.js"
+  }
+]);
